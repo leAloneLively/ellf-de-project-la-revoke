14ac2c600c29c859a39386a35bc68ea34d0caa9d
leAloneLively <amrtansh1456@gmail.com>
2025-12-17 20:04:07 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_on_chat(cmd, player2, pass2):
+    if pass2 == "admin password?":
+        pass
+player.on_chat("/admin", on_on_chat)
---
+++ main.ts
@@ -0,0 +1,6 @@
+player.onChat("/admin", function on_on_chat(cmd: number, player2: number, pass2: number) {
+    if (pass2 == "admin password?") {
+        
+    }
+    
+})
